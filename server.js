c26f6db078a5f88a20188e68377708dfabf65c55
hillkai <k.deehdance@gmail.com>
2018-07-31 15:28:22 -0700

    Created a public folder. Moved some files into the public folder. Added handlebars.

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var fs = require('fs'); //this is for reading files and will be helpful later
-var http = require('http');
+var express = require('express');
+var app = express();
 
+//this is a middleware function. It uses HTTP GET method. It returns whatever our fuction says to when a user wants to go to that specific place.
+//they are also known as routes
 app.get('/', function(req, res){
-  res.status(200).render('');
+  res.render("home.html");
 });
 app.use(express.static('public'));
-app.listen(port, function(){
-  console.log("== Server is listening on port", port);
+//this is a callback function to let us know the server has successfully started
+app.listen(3000, function(){
+  console.log("== Server is listening to port 3000");
 });
