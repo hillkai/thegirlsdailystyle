0236e9065f4aec86dbf9350a44bce1e64b0857a6
hillkai <k.deehdance@gmail.com>
2018-08-01 12:35:53 -0700

    added express-handlebars

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 //this is a middleware function. It uses HTTP GET method. It returns whatever our fuction says to when a user wants to go to that specific place.
 //they are also known as routes
 app.get('/', function(req, res){
-  res.render("home.html");
+  res.render("home");
 });
 app.use(express.static('public'));
 //this is a callback function to let us know the server has successfully started
