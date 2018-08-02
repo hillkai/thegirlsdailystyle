50273ffd7247faac52585312b2ace8b3bea483db
hillkai <k.deehdance@gmail.com>
2018-08-02 12:37:18 -0700

    now serving fave items with information coming from json file

--- server.js
+++ server.js
@@ -5,6 +5,53 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+//Read in Items Json file
+var data = fs.readFileSync('information/iteminformation.json');
+data = String(data);
+data = data.replace(/\r/g, "");
+//Put first word from file in array of strings to be added to Item object
+var stringToAddToObj = get_information(data, 0);
+var arrOfItemObjContent = [];
+arrOfItemObjContent.push(stringToAddToObj);
+//add the rest of the words to the array of strings
+for(var i=stringToAddToObj.length-1; i<data.length-1; i++){
+  if(data[i] == '\n'){
+    i++;
+    stringToAddToObj = get_information(data, i);
+    arrOfItemObjContent.push(stringToAddToObj);
+    i += stringToAddToObj.length-1;
+  }
+}
+var itemObj = createItemObj(arrOfItemObjContent);
+function get_information(stringToParse, startPointInString){
+  var stringToReturn = "";
+  var counter = startPointInString;
+  while(stringToParse[counter] != '\n'){
+    stringToReturn += stringToParse[counter];
+    counter++;
+  }
+  return stringToReturn;
+}
+//take the array of strings of the item information and put them into the array
+//of objects that will be used in my middleware function
+function createItemObj(arrOfItemObjContent){
+  var itemObj = [];
+  var counter = 0;
+  counter2 = 0;
+  for(var i=0; i<arrOfItemObjContent.length; i++){
+    var singleItemObj = {ItemImagePath: "", ItemBrand: "", ItemLinkActual: "", ItemLinkShown: ""};
+    singleItemObj.ItemImagePath = arrOfItemObjContent[i];
+    i++;
+    singleItemObj.ItemBrand = arrOfItemObjContent[i];
+    i++;
+    singleItemObj.ItemLinkActual = arrOfItemObjContent[i];
+    i++;
+    singleItemObj.ItemLinkShown = arrOfItemObjContent[i];
+    itemObj.push(singleItemObj);
+  }
+  return itemObj;
+}
+
 //this is a middleware function. It uses HTTP GET method. It returns whatever our fuction says to when a user wants to go to that specific place.
 //they are also known as routes
 var Home = new Array(1);
@@ -32,12 +79,11 @@ app.get('/aboutus', function(req, res){
     PageTitle: "ABOUT US"
   });
 });
-var Items = new Array(1);
-Items[0] = {ImagePath: "/photos/AGQM7548.JPG", ItemBrand: "Forever 21 cashmere sweater", ItemLink: "www.forever21.com", ItemLinkShown: "forever21.com" }
+
 app.get('/faveitems', function(req, res){
   res.status(200).render('items',{
     PageTitle: "FAVE ITEMS",
-    ItemsInfo: Items
+    ItemsInfo: itemObj
   });
 });
 
@@ -52,7 +98,6 @@ app.get('/navblog', function(req, res){
 
 var Blog = new Array(1);
 Blog[0] = {BlogTitle: "Best Blog", BlogText: "this is osme text", BlogPhoto: "/photos/AGQM7548.JPG"};
-console.log("Length: ", Blog.length);
 app.get('/blog', function(req, res){
   res.status(200).render('blog', {
     PageTitle: "BLOG",
